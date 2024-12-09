bb5c739f8041c73deb4257c6b857572e0bbb4c61
Kaiky Federsoni Martins <147348962+kaikyfedersoni@users.noreply.github.com>
2024-12-09 12:44:52 -0300

    Update useCarroDataMutate.ts

--- src/hooks/useCarroDataMutate.ts
+++ src/hooks/useCarroDataMutate.ts
@@ -2,7 +2,7 @@ import axios, { AxiosPromise } from "axios"
 import { CarroData } from "../interface/CarroData";
 import { useMutation} from "@tanstack/react-query";
 
-const API_URL = 'http:/34.204.143.229:8080'
+const API_URL = 'http://34.204.143.229:8080'
 
 const postData = async(data:CarroData): AxiosPromise<any> => {
     const response = axios.post(API_URL + '/carro',data);
