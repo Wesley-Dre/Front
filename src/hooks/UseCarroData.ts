c0964e31c22d3a259ef51fe2b62096f4492f44f2
Kaiky Federsoni Martins <147348962+kaikyfedersoni@users.noreply.github.com>
2024-12-10 09:25:56 -0300

    Update UseCarroData.ts

--- src/hooks/UseCarroData.ts
+++ src/hooks/UseCarroData.ts
@@ -4,10 +4,10 @@ import { useQuery } from "@tanstack/react-query";
 
 const API_URL = 'http://34.204.143.229:8080'
 
-const fetchData = async(): AxiosPromise<CarroData[]> => {
-    const response = axios.get(API_URL + '/api/carro/listar');
+const fetchData = async (): AxiosPromise<CarroData[]> => {
+    const response = await axios.get('${API_URL}/api/carro/listar');
     return response;
-}
+};
 export function useCarroData(){
 const query = useQuery({
     queryFn: fetchData,
